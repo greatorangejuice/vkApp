6fd469cda5222b91988bdeb1f6008121eee74e96
Pavel Snigirev <psnigirev@elinext.com>
2021-07-28 11:40:29 +0300

    Updates

---
+++ precache-manifest.fb7bd4b2cc3ddb3aad837bb04e7c275b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3133b7fd53357a804305397fea5aab5a",
+    "url": "/vkApp/index.html"
+  },
+  {
+    "revision": "47d75632eed5944d2cc3",
+    "url": "/vkApp/static/css/2.18f36260.chunk.css"
+  },
+  {
+    "revision": "47d75632eed5944d2cc3",
+    "url": "/vkApp/static/js/2.2be877ba.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/vkApp/static/js/2.2be877ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df514cb35f3170ff4a21",
+    "url": "/vkApp/static/js/main.8a4a782b.chunk.js"
+  },
+  {
+    "revision": "0f9427087a8abb1d1764",
+    "url": "/vkApp/static/js/runtime-main.61c462d9.js"
+  }
+]);
